53468c6ed0f21d4afce11bc7c3ce9fa46a9ee8bb
ot07 <ot07neoki@gmail.com>
2023-04-18 20:57:12 +0900

    install prettier plugins

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,11 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  semi: false,
+  singleQuote: true,
+  plugins: [
+    require.resolve("prettier-plugin-tailwindcss"),
+    require.resolve("prettier-plugin-organize-imports")
+  ],
 };
 
 module.exports = config;
