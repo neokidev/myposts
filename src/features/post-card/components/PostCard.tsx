0efc9276d23c08dbf959166437932fef14294ea4
ot07 <ot07neoki@gmail.com>
2023-04-26 23:58:41 +0900

    fix PostCard nested link

--- src/features/post-card/components/PostCard.tsx
+++ src/features/post-card/components/PostCard.tsx
@@ -14,37 +14,42 @@ export type PostCardProps = {
 export const PostCard: FC<PostCardProps> = ({ post }) => {
   return (
     <article className="overflow-hidden rounded-lg border shadow-lg group">
-      <Link href="#">
-        <div className="relative h-40 overflow-hidden">
-          <Image
-            alt="Placeholder"
-            src="https://picsum.photos/600/400/?random"
-            fill
-            className="group-hover:scale-125 ease-in-out duration-300"
-          />
-        </div>
-        <div className="p-4 space-y-3">
-          <h1 className="text-xl font-bold line-clamp-3 group-hover:text-gray-600">
+      <Link href="#" className="relative h-40 overflow-hidden block">
+        <Image
+          alt="Placeholder"
+          src="https://picsum.photos/600/400/?random"
+          fill
+          className="group-hover:scale-125 ease-in-out duration-300"
+        />
+      </Link>
+      <div className="p-4 space-y-3">
+        <Link href="#">
+          <h1 className="inline text-xl font-bold line-clamp-3 hover:text-gray-600">
             {post.title}
           </h1>
+        </Link>
+        <div className="flex items-center space-x-2">
           <Link
-            className="flex items-center no-underline hover:underline space-x-1.5"
             href="#"
+            className="relative w-7 h-7 rounded-full overflow-hidden"
           >
-            <div className="relative w-7 h-7 rounded-full overflow-hidden">
-              <Image
-                alt="Placeholder"
-                src="https://picsum.photos/32/32/?random"
-                fill
-              />
-            </div>
-            <p className="text-sm">{post.authorName}</p>
+            <Image
+              alt="Placeholder"
+              src="https://picsum.photos/32/32/?random"
+              fill
+            />
           </Link>
-          <div className="text-xs text-gray-500 font-light">
-            {dayjs(post.createdAt).fromNow()}
-          </div>
+          <Link
+            href="#"
+            className="inline text-sm no-underline hover:underline "
+          >
+            {post.authorName}
+          </Link>
         </div>
-      </Link>
+        <div className="text-xs text-gray-500 font-light">
+          {dayjs(post.createdAt).fromNow()}
+        </div>
+      </div>
     </article>
   )
 }
